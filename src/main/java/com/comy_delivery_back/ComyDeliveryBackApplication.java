de0aafa3ccb71381e57baa8bbbf6073cb3f18c25
Heloisa Tavares <heloisamt@gmail.com>
2025-11-11 16:13:25 -0300

    :tada: First Commit

---
+++ src/main/java/com/comy_delivery_back/ComyDeliveryBackApplication.java
@@ -0,0 +1,13 @@
+package com.comy_delivery_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComyDeliveryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComyDeliveryBackApplication.class, args);
+	}
+
+}
