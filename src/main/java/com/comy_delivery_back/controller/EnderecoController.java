a84617496d4c512881a03d041cef28e9fae082b9
Heloisa Tavares <heloisamt@gmail.com>
2025-11-13 14:01:58 -0300

    Settando as classes de endereço

--- src/main/java/com/comy_delivery_back/controller/EnderecoController.java
+++ src/main/java/com/comy_delivery_back/controller/EnderecoController.java
@@ -1,4 +1,23 @@
 package com.comy_delivery_back.controller;
 
+import com.comy_delivery_back.client.ApiCepClient;
+import com.comy_delivery_back.dto.response.ApiCepDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/endereco")
 public class EnderecoController {
+
+    private final ApiCepClient apiCepClient;
+
+    public EnderecoController(ApiCepClient apiCepClient) {
+        this.apiCepClient = apiCepClient;
+    }
+
+    @GetMapping("/{cep}")
+    public ResponseEntity<ApiCepDTO> buscarPorCep(@PathVariable("cep") String cep) {
+        return this.apiCepClient.buscarCep(cep);
+    }
 }
