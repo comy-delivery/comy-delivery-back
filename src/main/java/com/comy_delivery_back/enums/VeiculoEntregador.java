14e139db30a1d0ebdf9f0e7f7396d98e1bd8a158
JUDE VIEIRA DE LIMA <jude_lima@estudante.sesisenai.org.br>
2025-11-13 14:04:19 -0300

    iniciando classe entregador // criação enum VeiculoEntregador

---
+++ src/main/java/com/comy_delivery_back/enums/VeiculoEntregador.java
@@ -0,0 +1,7 @@
+package com.comy_delivery_back.enums;
+
+public enum VeiculoEntregador {
+    MOTO,
+    BICICLETA,
+    CARRO
+}
--- src/main/java/com/comy_delivery_back/model/Entregador.java
+++ src/main/java/com/comy_delivery_back/model/Entregador.java
@@ -1,20 +1,51 @@
 package com.comy_delivery_back.model;
 
+import com.comy_delivery_back.enums.VeiculoEntregador;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Entregador{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idEntregador;
+@PrimaryKeyJoinColumn(name = "idUsuario")
+public class Entregador extends Usuario{
 
-    @OneToOne
-    @MapsId
-    private Usuario usuario;
+    @Column(nullable = false)
+    private String nmEntregador;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 11)
+    private String telefone;
+
+    @Column(length = 11, nullable = false, unique = true)
+    private String cpf;
+
+    @Enumerated(EnumType.STRING)
+    private VeiculoEntregador veiculo;
+
+    @Column(nullable = false)
+    private String placa;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean isDisponivel;
+
+    private Double avaliacaoMedia;
+
+    @OneToMany(
+            mappedBy = "idUsuario",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Entrega> entregas;
+
+    @Column(columnDefinition = "boolean default true")
+    private boolean isAtivo;
+
 }
