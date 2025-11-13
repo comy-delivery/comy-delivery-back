e2a683bff8f1c44c3c84c3774a7abfcea11e5c81
JUDE VIEIRA DE LIMA <jude_lima@estudante.sesisenai.org.br>
2025-11-13 16:37:14 -0300

    criação da classe Admin

---
+++ src/main/java/com/comy_delivery_back/model/Admin.java
@@ -0,0 +1,30 @@
+package com.comy_delivery_back.model;
+
+import com.comy_delivery_back.enums.RoleUsuario;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@PrimaryKeyJoinColumn(name = "idUsuario")
+public class Admin extends Usuario{
+    public Admin(){
+        this.setRoleUsuario(RoleUsuario.ADMIN);
+    }
+
+    @Column(nullable = false)
+    private String nomeAdmin;
+
+    @Column(unique = true, nullable = false)
+    private String cpfAdmin;
+
+    @Column(unique = true, nullable = false)
+    private String emailAdmin;
+
+}
