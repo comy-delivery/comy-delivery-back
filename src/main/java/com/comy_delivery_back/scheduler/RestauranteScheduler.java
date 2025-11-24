02d8777ef2f3711257603e924f4e8b8b3b7affe8
judevieira <judevieiradlima@gmail.com>
2025-11-24 00:15:52 -0300

    adicionando a classe RestauranteScheduler e ajustando service

---
+++ src/main/java/com/comy_delivery_back/scheduler/RestauranteScheduler.java
@@ -0,0 +1,23 @@
+package com.comy_delivery_back.scheduler;
+
+import com.comy_delivery_back.service.RestauranteService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestauranteScheduler {
+
+    private final RestauranteService restauranteService;
+
+    public RestauranteScheduler(RestauranteService restauranteService) {
+        this.restauranteService = restauranteService;
+    }
+
+    //executa a cada 5 min
+    @Scheduled(cron = "0 0/5 * * * *")
+    public void verificarHorariosRestaurantes() {
+        System.out.println("SCHEDULER: Verificando e atualizando status de abertura dos restaurantes. (A cada 5 min)");
+
+        restauranteService.atualizarStatusAberturaFechamento();
+    }
+}
