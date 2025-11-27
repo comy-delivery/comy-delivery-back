fc6ec439d1246277635791d453c638e65c9adc97
JUDE VIEIRA DE LIMA <jude_lima@estudante.sesisenai.org.br>
2025-11-27 14:14:33 -0300

    finalizacao (eu acho) do oauth2

--- src/main/java/com/comy_delivery_back/controller/AuthController.java
+++ src/main/java/com/comy_delivery_back/controller/AuthController.java
@@ -4,12 +4,16 @@ import com.comy_delivery_back.dto.request.ClienteRequestDTO;
 import com.comy_delivery_back.dto.request.EntregadorRequestDTO;
 import com.comy_delivery_back.dto.request.LoginRequestDTO;
 import com.comy_delivery_back.dto.request.SignupRequestDTO;
+import com.comy_delivery_back.dto.response.ClienteResponseDTO;
+import com.comy_delivery_back.dto.response.EntregadorResponseDTO;
 import com.comy_delivery_back.dto.response.LoginResponseDTO;
 import com.comy_delivery_back.dto.response.SignupResponseDTO;
+import com.comy_delivery_back.model.Cliente;
 import com.comy_delivery_back.service.AuthService;
 import com.comy_delivery_back.service.ClienteService;
 import com.comy_delivery_back.service.EntregadorService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,15 +39,33 @@ public class AuthController {
         return ResponseEntity.ok(authService.login(loginRequestDTO)); //mudar depois
     }
 
-    @PostMapping("register-complete")
+    @PostMapping("/register-complete")
     public ResponseEntity<?> completeRegistration(@RequestParam String role,
                                                   @RequestBody Map<String, Object> dadosRegistro){
         if (!role.equalsIgnoreCase("CLIENTE") && !role.equalsIgnoreCase("ENTREGADOR")){
             return ResponseEntity.badRequest().body("Role inválida. Escolha CLIENTE ou ENTREGADOR.");
         }
+        try{
+            if (role.equalsIgnoreCase("CLIENTE")){
+                ClienteRequestDTO clienteRequestDTO = objectMapper.convertValue(dadosRegistro, ClienteRequestDTO.class);
 
-        if (role.equalsIgnoreCase("CLIENTE")){
-            ClienteRequestDTO clienteRequestDTO = objectMapper.convertValue(dadosRegistro, ClienteRequestDTO.class)
+                ClienteResponseDTO novoCliente = clienteService.cadastrarCliente(clienteRequestDTO);
+
+                return ResponseEntity.status(HttpStatus.CREATED).body(novoCliente);
+
+            }else if (role.equalsIgnoreCase("ENTREGADOR")){
+                EntregadorRequestDTO entregadorRequestDTO = objectMapper.convertValue(dadosRegistro, EntregadorRequestDTO.class);
+
+                EntregadorResponseDTO entregadorResponseDTO = entregadorService.cadastrarEntregador(entregadorRequestDTO);
+
+                return ResponseEntity.status(HttpStatus.CREATED).body(entregadorResponseDTO);
+            }
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Erro de cadastro: " + e.getMessage()); //erro de cadastro ex: cpf existe
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Erro interno no processamento do cadastro: " + e.getMessage()); //erro de servidor
+
         }
+        return ResponseEntity.internalServerError().body("Erro genérico");
     }
 }
